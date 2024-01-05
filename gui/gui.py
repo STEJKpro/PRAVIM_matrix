fcd21230a159bf0b47909358c32360fd231a60d4
STEJKpro <su_dan@bk.ru>
2024-01-06 01:23:02 +0300

    Добавлен новый функционал | v 1.1

--- gui/gui.py
+++ gui/gui.py
@@ -1,86 +1,164 @@
-# import customtkinter
-# import os
-    
-
-# class App(customtkinter.CTk):
-#     def __init__(self):
-#         super().__init__()
-
-#         # configure window
-#         self.title("Генератор планограм")
-#         self.geometry(f"{1100}x{580}")
-        
-#         # configure grid layout (4x4)
-#         self.grid_columnconfigure(1, weight=1)
-#         self.grid_columnconfigure((2, 3), weight=0)
-#         self.grid_rowconfigure((0, 1, 2), weight=1)
-        
-#         self.label = customtkinter.CTkLabel(self, text="Генератор планограм", fg_color="transparent")
-#         self.label.grid(row=0, column=0, rowspan=1, sticky="nsew")
-#         self.label.grid_rowconfigure(4, weight=1)
-
-
-#         # create home frame
-#         self.home_frame_button_1 = customtkinter.CTkButton(self, text=" Сгенерировать",)
-#         self.home_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
-
-
-# if __name__ == "__main__":
-#     customtkinter.set_appearance_mode("dark")
-#     app = App()
-    # app.mainloop()
 from matrix_generator import excel_generator
 import customtkinter
 from tkinter.filedialog import asksaveasfile
+import pyperclip
 
+class TopWindow:
+    def __init__(self, root, lable_text):
+        self.root = root
+        self.window = customtkinter.CTkToplevel()
+        self.window.title('Сообщение')
+        # self.window.geometry ("300x100")
+        self.window.geometry("%dx%d+%d+%d" % (300, 100, root.winfo_x() + 700/4, root.winfo_y() + 550/4))
+         
+        label = customtkinter.CTkLabel(self.window, text=lable_text)
+        label.pack()
+         
+        btn = customtkinter.CTkButton(self.window, text='Окей', command=self.close)
+        btn.pack(anchor='s')
+        self.root.withdraw()
+    def close(self):
+        self.root.deiconify()
+        self.window.destroy()
+        
 
 class App(customtkinter.CTk):
+
+    def keypress(self, e):
+        if e.keycode == 86 and e.keysym != 'v':
+            e.widget.event_generate("<<Paste>>")
+            
+        elif e.keycode == 67 and e.keysym != 'c':
+            e.widget.event_generate("<<Copy>>")
+            
+        elif e.keycode == 88 and e.keysym != 'x':
+            e.widget.event_generate("<<Сut>>")
+            
+        elif e.keycode == 65 and e.keysym != 'x':
+            e.widget.event_generate("<<SelectAll>>")
+            
+    def get_signature_text_from_file(self,) -> str:
+        with open('gui\src\signature_text.txt', 'r', encoding='utf-8') as file:
+            return file.read()
+             
+    def update_signature_text_to_file(self, signature) -> None:
+        with open('gui\src\signature_text.txt', 'w', encoding='utf-8') as file:
+            file.write(signature)
+      
     def __init__(self):
         super().__init__()
-
-        self.title("image_example.py")
-        self.geometry("700x450")
+        self.title("Генератор планограммы (ассортиментной матрицы)")
+        self.geometry("700x750")
 
         # set grid layout 1x2
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
         customtkinter.set_appearance_mode('dark')
+        self.bind("<Control-KeyPress>", self.keypress)
+        # self.bind("<Control-86>", lambda: print('Okkkkkk'))
+
+        
 
 
         # create home frame
         self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.home_frame.grid_columnconfigure(0, weight=1)
 
+
+
         self.home_frame_large_image_label = customtkinter.CTkLabel(self.home_frame, text="Генератор планограмм для Haiba/Cron",)
         self.home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
         
         #Add textbox
-        self.textbox = customtkinter.CTkTextbox(master=self.home_frame, width=400, corner_radius=0, font = ('',20))
+        self.textbox = customtkinter.CTkTextbox(master=self.home_frame, width=400, height=150, corner_radius=0, font = ('',16),)
         self.textbox.grid(row=0, column=0, sticky="nsew", padx=20, pady=10)
         self.textbox.insert("0.0", "HB101, HB101, HB101, HB10590-7, HB10803-7, HB10505-3\nHB101, HB101, HB101, HB60590-7, HB22803-7, HB60505-3\nHB10505, HB10505-8, HB10198, HB10590, CN10523, CN10129\nHB60505, HB60505-8, HB60198, HB60590, CN60523, CN22129")
         
-        self.slider = customtkinter.CTkSlider(master=self.home_frame, from_=100, to=300, command=self.set_slider_lable_text, number_of_steps=40)
+        #button to clear textbox
+        self.clear_button = customtkinter.CTkButton(master=self.home_frame, text="Очистить поле", command=lambda: self.textbox.delete('0.0','end'))
+        self.clear_button.grid(row=0, column=0, padx=20, pady=10, sticky='ne' )
+        
+        
+        self.slider = customtkinter.CTkSlider(master=self.home_frame, from_=125, to=195, command=self.set_slider_lable_text, number_of_steps=14)
         self.slider.grid(row=2, column=0, sticky="nwe", padx=10, pady=10)
         
-        self.slider_label = customtkinter.CTkLabel(self.home_frame, text=f"Размер изображений\n135 - смесители, 160 - ДП.\n Текущее значение: {int(self.slider.get())}", justify='left')
+        self.slider_label = customtkinter.CTkLabel(
+            self.home_frame,
+            text=f"Размер изображений\n135 - смесители, 160 - ДП.\n Текущее значение: {int(self.slider.get())}",
+            justify='left',
+            font=('', 16),
+        )
         self.slider_label.grid(row=1, column=0, sticky="sw", padx=10, pady=10, ) 
 
-        # slider = customtkinter.CTkSlider(app, from_=0, to=100, command=slider_event)
+
+        self.customer_field = customtkinter.CTkEntry(self.home_frame, placeholder_text="Контрагент", font=('', 20))
+        self.customer_field.grid(row=3, column=0, sticky="nsew", padx=10, pady=20, ) 
+        
+        self.signature_switch = customtkinter.CTkSwitch(
+            self.home_frame,
+            text="Добавить подпись\n ** - Выделить жирным",
+            font=('', 16),
+            command=lambda: print(self.signature_switch.get())
+        )
+        self.signature_switch.grid(row=4, column=0, sticky="nsew", padx=10, pady=(20,)) 
         
         
-        self.home_frame_button_1 = customtkinter.CTkButton(self.home_frame, text="Сгенрировать!", height=50, font=('', 32), command= lambda: save_file())
-        self.home_frame_button_1.grid(row=3, column=0, padx=20, pady=10, ipadx = 20, ipady = 10)
+        self.signature_field = customtkinter.CTkTextbox(self.home_frame, height=120, font =('', 16))
+        self.signature_field.grid(row=5, column=0, sticky="nsew", padx=10, pady=(0,20) )
+        self.signature_field.insert('0.0', str(self.get_signature_text_from_file()))
+        
+        self.save_signature_button = customtkinter.CTkButton(
+            master=self.home_frame, 
+            text="Сохранить подпись", 
+            command=lambda: self.update_signature_text_to_file(self.signature_field.get("1.0", 'end-1c').strip())
+        )
+        self.save_signature_button.grid(row=5, column=0, padx=20, pady=0, sticky='ne' )
+        
+        
+        self.update_switch = customtkinter.CTkSwitch(
+            self.home_frame,
+            text="Обновить прайс. \n(Если включено, то скачает прайс-лист из облака. \nИначе - использует ранее скачанные)",
+            font=('', 16),
+        )
+        self.update_switch.grid(row=6, column=0, sticky="nsew", padx=10, pady=20, ) 
+        
+        self.home_frame_button_1 = customtkinter.CTkButton(self.home_frame, text="Сгенерировать и сохранить!", height=50, font=('', 26), command= lambda: self.save_file())
+        self.home_frame_button_1.grid(row=7, column=0, padx=20, pady=10, ipadx = 20, ipady = 10)
+        
         self.home_frame.grid(row=0, column=1, sticky="nsew")
 
+
+        
     def set_slider_lable_text(self, value):
         self.slider_label.configure(text = f"Размер изображений\n135 - смесители, 160 - ДП\n Текущее значение: {int(value)}")
     
-def save_file():
-    f = asksaveasfile(initialfile = 'Файл_без_имени.xlsx',
-        defaultextension=".xlsx",filetypes=[("All Files","*.*"),("Excel files","*.xlsx")], mode='wb')
-    f.write(excel_generator.generate_excel_file())
-    f.close
-        
+    def parse_matrix (self, string) -> list:
+        matrix = []
+        for line in string.split('\n'):
+                matrix.append ([x.strip() for x in line.strip().split(',')])
+        return matrix
+
+    
+    def save_file(self):
+        try:
+            f = asksaveasfile(initialfile = 'Файл_без_имени.xlsx',
+                defaultextension=".xlsx",filetypes=[("All Files","*.*"),("Excel files","*.xlsx")], mode='wb')
+            f.write(
+                excel_generator.generate_excel_file(
+                    matrix= self.parse_matrix(self.textbox.get("1.0", 'end-1c').strip()),
+                    img_size=int(self.slider.get()),
+                    customer=self.customer_field.get(),
+                    signature_text = self.signature_field.get("1.0", 'end-1c').strip() if self.signature_switch.get() == 1 else None,
+                    get_updates = int(self.update_switch.get()),
+                )
+            )
+            TopWindow(self, 'Файл успешно сохранен!')
+        except Exception as ex:
+            TopWindow(self, f'Произошла ошибка:\n {ex}')
+        finally:
+            f.close
+            
+
 if __name__ == "__main__":
     app = App()
     app.mainloop()
--- matrix_generator/excel_generator.py
+++ matrix_generator/excel_generator.py
@@ -4,16 +4,19 @@ import pandas as pd
 import os
 from PIL import Image as PIL_Image
 
-from openpyxl import Workbook
+from openpyxl import Workbook, comments
 from openpyxl.drawing.image import Image
 from openpyxl.utils import get_column_letter
 from openpyxl.drawing.spreadsheet_drawing import OneCellAnchor, AnchorMarker
 from openpyxl.drawing.xdr import  XDRPositiveSize2D
 from openpyxl.utils.units import pixels_to_EMU as p2e, pixels_to_points
 from openpyxl.styles import Border, Side, Alignment, Font
-
+from openpyxl.cell.rich_text import TextBlock, CellRichText
+from openpyxl.cell.text import InlineFont
+import re
 from io import BytesIO
 
+from datetime import date
 IMG_OFFSET = 2 #Отступ в пикселях от угла ячейки к которой приязано изображение (использовать меньше 2 не рекомендуется)
 
 ATRR_NAMES = ['Торговая марка', 'Артикул', 'Материал', 'Цвет', 'Розничная цена (ориентировочно), BYN']
@@ -27,46 +30,78 @@ def resized_image(size,img_url):
         image = PIL_Image.open(requests.get(img_url, stream = True).raw)
         image.thumbnail((size,size), PIL_Image.LANCZOS)
         image.save(cached_image_path)
-        print(cached_image_path)
-    return cached_image_path
+    return cached_image_path, img_url
 
 
-def get_pricelist(urls=[
-    'https://docs.google.com/spreadsheets/d/1MxIv111Z1NnP6qbcJ_QIL5FM8IWlp4qAn3ifCUGZooM/export?format=csv&id=1MxIv111Z1NnP6qbcJ_QIL5FM8IWlp4qAn3ifCUGZooM&gid=1949199208',
-    'https://docs.google.com/spreadsheets/d/1MxIv111Z1NnP6qbcJ_QIL5FM8IWlp4qAn3ifCUGZooM/export?format=csv&id=1MxIv111Z1NnP6qbcJ_QIL5FM8IWlp4qAn3ifCUGZooM&gid=125708755'
-                ]):
+def get_pricelist(
+    urls=[
+        'https://docs.google.com/spreadsheets/d/1MxIv111Z1NnP6qbcJ_QIL5FM8IWlp4qAn3ifCUGZooM/export?format=csv&id=1MxIv111Z1NnP6qbcJ_QIL5FM8IWlp4qAn3ifCUGZooM&gid=1949199208',
+        'https://docs.google.com/spreadsheets/d/1MxIv111Z1NnP6qbcJ_QIL5FM8IWlp4qAn3ifCUGZooM/export?format=csv&id=1MxIv111Z1NnP6qbcJ_QIL5FM8IWlp4qAn3ifCUGZooM&gid=125708755'
+    ],
+    get_updates=1
+):
     df = pd.DataFrame()
-    for url in urls:
-        response = requests.get(url)
-        with open(f'price_{urls.index(url)}.csv', 'wb') as file:
-            file.write(response.content)
-        df = pd.concat([df, pd.read_csv(url, encoding='utf-8', usecols=['Артикул','РРЦ с НДС, BYN (справочно)', 'Фото', 'Бренд', 'Материал', 'Цвет'], index_col='Артикул')])
-    return df
+    if get_updates == 1:       
+        for url in urls:
+            response = requests.get(url)
+            with open(f'matrix_generator/price/price_{urls.index(url)}.csv', 'wb') as file:
+                file.write(response.content)
+            df = pd.concat([df, pd.read_csv(url, encoding='utf-8', usecols=['Артикул','РРЦ с НДС, BYN (справочно)', 'Фото', 'Бренд', 'Материал', 'Цвет'], index_col='Артикул')])
+        return df
+    if get_updates == 0:
+        for root, dirs, files in os.walk('./matrix_generator/price/'):
+            for name in files:
+                if name.startswith('price_'):
+                    df = pd.concat([df, pd.read_csv(os.path.join(root, name), encoding='utf-8', usecols=['Артикул','РРЦ с НДС, BYN (справочно)', 'Фото', 'Бренд', 'Материал', 'Цвет'], index_col='Артикул')])
+        return df
+    else: raise FileNotFoundError('Файлы с прайс-листами не найдены')
 
-def parse_matrix (file_path = 'matrix_generator/matrix.txt'):
-    matrix = []
-    img_size = None
-    customer = None
-    with open(file_path, 'r', encoding='utf-8') as file:
-        for line in file.readlines():
-            if line.startswith('Контрагент'): 
-                customer = line.split(':')[-1].strip()
-            elif line.startswith('Размер изображения'): 
-                try:
-                    img_size = int(line.split(':')[-1].strip())
-                except ValueError:
-                    img_size = None
-            else:
-                matrix.append ([x.strip() for x in line.strip().split(',')])
-                
-                
-    return matrix, customer, img_size
+def markdown_to_richtext(text):
+    # Создаем пустой объект CellRichText
+    richtext = CellRichText()
+
+    # Определяем шаблоны для разных стилей markdown
+    bold_pattern = r"\*\*([^\*]+)\*\*"
+    # Определяем стили для разных шаблонов
+    bold_style = InlineFont(b=True)
+    # italic_style = InlineFont(i=True)
 
 
-matrix, customer, img_size = parse_matrix()\
-    
-def generate_excel_file(matrix, customer, img_size):
-    pricelist = get_pricelist()  
+    # Ищем все совпадения с шаблонами в строке с markdown
+    bold_matches = re.finditer(bold_pattern, text)
+    # italic_matches = re.finditer(italic_pattern, markdown)
+
+    # Создаем список из всех совпадений с их индексами и стилями
+    matches = []
+    for match in bold_matches:
+        matches.append((match.start(), match.end(), match.group(1), bold_style))
+    # for match in italic_matches:
+    #     matches.append((match.start(), match.end(), match.group(1), italic_style))
+    # for match in strike_matches:
+    #     matches.append((match.start(), match.end(), match.group(1), strike_style))
+    # Сортируем список по индексам совпадений
+    matches.sort()
+
+    # Добавляем в объект CellRichText фрагменты текста с соответствующими стилями
+    start = 0
+    for match in matches:
+        # Добавляем обычный текст до совпадения
+        if len(text[start:match[0]]) > 0:
+            richtext.append(str(text[start:match[0]]))
+        # Добавляем текст совпадения с нужным стилем
+        richtext.append(TextBlock(text = match[2], font = match[3]))
+        # Обновляем индекс начала следующего фрагмента
+        start = match[1]
+    # Добавляем оставшийся текст после последнего совпадения
+    richtext.append( str(text[start:]))
+
+    # Возвращаем объект CellRichText
+    return richtext
+
+ 
+def generate_excel_file(matrix, customer:str = None, img_size:int = None, signature_text = None,get_updates=1):
+    print(signature_text)
+    pricelist = get_pricelist(get_updates=get_updates)  
     for row in matrix:
         for i, item in enumerate(row):
             values = pricelist.loc[item]
@@ -76,7 +111,7 @@ def generate_excel_file(matrix, customer, img_size):
                 'vendor_code': item,
                 'material': values.get('Материал'),
                 'color': values.get('Цвет'),
-                'price': values.get('РРЦ с НДС, BYN (справочно)'),
+                'price': values.get('РРЦ с НДС, BYN (справочно)'),                
             }
 
     wb = Workbook()
@@ -85,8 +120,39 @@ def generate_excel_file(matrix, customer, img_size):
     bd = Side(style='thin', color="000000")
     border_style = Border(left=bd, top=bd, right=bd, bottom=bd)
     alignment=Alignment(horizontal='center')
+    
+    
+    #### Вставка шакпки с лого
+    marker = AnchorMarker(col=2, colOff=p2e(IMG_OFFSET), row=1, rowOff=p2e(IMG_OFFSET))
+    logo = Image ('matrix_generator/pravim_logo.png')
+    h, w = logo.height, logo.width
+    ws.row_dimensions[2].height = pixels_to_points(h+2*IMG_OFFSET+2)
+    size = XDRPositiveSize2D(p2e(w), p2e(h))
+    logo.anchor = OneCellAnchor(_from=marker, ext=size)
+    ws.add_image(logo)
+    
+    #Вставка контрагента и даты создания в шапку
+    bold_inline = InlineFont(b=True)
+    value = CellRichText(
+        [
+            TextBlock(font = bold_inline, text = '- Подготовлено для: ') if customer else '',
+            str(customer)+'\n' if customer else '',
+            TextBlock(font = bold_inline, text = '- Создано: '),
+            str(date.today().strftime('%d.%m.%Y')),
+        ]
+    )
+    ws['B2'].alignment = Alignment(wrap_text=True)    
+    ws['B2'] = value
+    
+    #Вставка текста подписи
+    if signature_text:
+        ws['F2'].alignment = Alignment(wrap_text=True)
+        ws['F2'] = markdown_to_richtext(signature_text)
+        ws.merge_cells('F2:H2')
+        ws.row_dimensions[2].height = len(signature_text.split('\n'))*15 if ws.row_dimensions[2].height < len(signature_text.split('\n'))*15 else pixels_to_points(h+2*IMG_OFFSET+2)
 
-    current_row = 2
+    #### Вставка планограммы
+    current_row = 3
     for matrix_row in matrix:
         current_collumn = 3
         #Ширина ячейки с наименованиями атрибутов 
@@ -96,13 +162,16 @@ def generate_excel_file(matrix, customer, img_size):
         for i, item in enumerate(matrix_row):
             # Вставка наименований атрибутов
             for name_i, name in enumerate(ATRR_NAMES):
-                ws.cell(row = current_row+name_i+1, column = 2, value= name).border = border_style
+                cell = ws.cell(row = current_row+name_i+1, column = 2, value= name).border = border_style
+                #В артикул добавляем примечание о кликабельности
+                if name == 'Артикул':
+                    cell.comment = comments.Comment('Клик по артикулу откроет картинку в полном размере',author='ООО "ПРАВИМ"')
                 
             
             # Цикл по атрибутам    
             for j, prop in enumerate(list(item.values())):
                 if j == 0:
-                    img = Image(prop)
+                    img = Image(prop[0])
                     
                     #Позиционирование изображение + добавление отступа для того чтобы были видны границы разметки
                     h, w = img.height, img.width
@@ -114,9 +183,10 @@ def generate_excel_file(matrix, customer, img_size):
 
                     ws.add_image(img)
                     
-                    cell = ws.cell(row = current_row+j, column = current_collumn)
+                    cell = ws.cell(row = current_row+j, column = current_collumn, value='')
                     cell.border = border_style
                     cell.alignment = alignment
+                    
                     
                     #Задаем ширину и высоту строки с изображением (числа подобраны исходя из размеров)
                     if not ws.column_dimensions[get_column_letter(current_collumn)].width or ws.column_dimensions[get_column_letter(current_collumn)].width < (w+2*IMG_OFFSET)/7:
@@ -127,12 +197,11 @@ def generate_excel_file(matrix, customer, img_size):
                     cell = ws.cell(row = current_row+j, column = current_collumn, value = prop)
                     cell.alignment = alignment
                     cell.border = border_style
+
                     
             current_collumn +=1
         current_row = current_row + len(item) +1
         
-    # Save the workbook
-    # wb.save('product_table_with_images.xlsx')
     virtual_workbook = BytesIO()
     wb.save(virtual_workbook)
     return virtual_workbook.getbuffer()
